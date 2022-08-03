1f00d9a7e0625498e7080fb4fe2730579be47ec1
Viacheslav <maverik2047@yandex.ru>
2022-08-03 23:41:16 +0300

    with lombok added

--- src/test/java/models/pojo/UserDataBodyModel.java
+++ src/test/java/models/pojo/UserDataBodyModel.java
@@ -1,35 +1,13 @@
 package models.pojo;
 
-public class UserDataBodyModel {
+import lombok.Getter;
+import lombok.Setter;
 
+
+@Getter
+@Setter
+public class UserDataBodyModel {
     private String name;
     private String job;
     private String email;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getJob() {
-        return job;
-    }
-
-    public void setJob(String job) {
-        this.job = job;
-    }
-
-
 }
--- src/test/java/models/pojo/UserDataResponseModel.java
+++ src/test/java/models/pojo/UserDataResponseModel.java
@@ -1,31 +1,16 @@
 package models.pojo;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 
 public class UserDataResponseModel {
-
-
     private String job;
     private String error;
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
-
-
-    public String getJob() {
-        return job;
-    }
-
-    public void setJob(String job) {
-        this.job = job;
-    }
-
 
 }
